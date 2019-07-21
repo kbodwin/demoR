fa4147ee874c6bb853d824c98b30949a55427f44
Kelly N. Bodwin <kbodwin@calpoly.edu>
2019-07-21 12:24:10 -0700

    better sammies

--- tests/testthat/test-split_sandwiches.R
+++ tests/testthat/test-split_sandwiches.R
@@ -2,21 +2,16 @@ test_that("split_sandwich pulls html apart", {
 
   highlighted_code <- "<pre class='r'><code>foo <- mean(<span style='background-color:#ffff7f'>1:10</span>)</code></pre>"
 
-  start_sym <- "\\>"
-  end_sym <- "\\<"
-
-  ignore_these <- c("\\<(?=(\\-|\\<))")
+  start_rx <- "\\<([^\\>\\<]|(\\>\\s*\\<))*([^\\-])\\>"
 
   expected_results_1 <- c(
-    "<pre class='r'>",
-    "<code>",
+    "<pre class='r'><code>",
     "foo <- mean(",
     "<span style='background-color:#ffff7f'>",
     "1:10",
     "</span>",
     ")",
-    "</code>",
-    "</pre>")
+    "</code></pre>")
 
-  expect_equal(expected_results_1, split_sandwiches(highlighted_code, start_sym, end_sym, ignore_these))
+  expect_equal(expected_results_1, split_sandwiches(highlighted_code, start_rx))
 })
