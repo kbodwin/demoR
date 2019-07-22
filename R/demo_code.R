ed904635da38f2ba4615b653cdf9b316b2dc68db
Kelly N. Bodwin <kbodwin@calpoly.edu>
2019-07-21 17:15:18 -0700

    none of this works but I need to save it

--- R/demo_code.R
+++ R/demo_code.R
@@ -1,61 +1,112 @@
 ## an object of type demo_code is a language object that has a print_string attribute
 
 #' @export
-demo_code <- function(.code, eval_here = TRUE) {
+demo_code <- function(.code_string, eval_here = TRUE) {
 
-  code_expr <- rlang::enexpr(.code)
+  .expr <- parse(text = .code_string)
 
   if (eval_here) {
 
-    code_expr %>%
-        deparse() %>%
-        str_replace_all("(?!=\\<)\\<\\-", "<<-") %>%
-        str_c(collapse = "") %>%
-        parse(text = .) %>%
-        eval()
+    scope_and_run(expr)
 
   }
 
-  print_string <- code_expr %>%
-    deparse() %>%
-    str_c(collapse = "<br>") %>%
-    str_replace_all("(^\\{\\s*)|(\\}$)", "") %>%
-    txt_tocode()
+  print_string <- .code_string %>%
+    str_trim() %>%
+    str_replace_all("\n", "<br>")
 
-  new_demo_code <- evaluate::evaluate(code_expr)
+  new_demo_code <- evaluate::evaluate(expr)
 
-  attr(new_demo_code, "class") <- "demo_code"
   attr(new_demo_code, "print_string") <- print_string
-  attr(new_demo_code, "expression") <- code_expr
+  attr(dc_list, "class") <- "demo_code"
 
-  return(new_demo_code)
+  return(dc_list)
 
 }
 
-
+#' @importFrom matahari dance_recital
 #' @export
-knit_print.demo_code <- function(x, ...) {
+demo_code_multi <- function(.code_string, eval_here = TRUE, shatter = TRUE) {
 
-  if (length(x) > 1) {
+  dc_list <- matahari::dance_recital(.code_string, evaluate = FALSE)
 
-    knitr::asis_output(paste(attr(x, "print_string"), knitr:::wrap(x[[2]], ...)))
+  if (eval_here) {
+
+    purrr::map(dc_list$expr, scope_and_run)
+
+  }
+
+  .code_string <- .code_string %>%
+    str_trim() %>%
+    str_replace_all("\n", "<br>")
+
+  if (shatter) {
+
+    print_strings <- str_split(.code_string, "((\\<br\\>){2,})|(;(\\<br\\>)*)") %>% unlist() %>% as.list()
 
   } else {
 
-    knitr::asis_output(attr(x, "print_string"))
+    print_strings <- as.list(c(.code_string, rep(NULL, nrow(dc_list) - 1)))
 
   }
 
+  dc_list$print_strings <- print_strings
+  dc_list$evaluations <- map(dc_list$expr, ~evaluate::evaluate(.x))
+
+  attr(dc_list, "class") <- "demo_code"
+
+  return(dc_list)
+
+}
+
+# eval_and_wrap <- function(.expr, ...) {
+#
+#   .expr <- enexpr(.expr)
+#   my_output <- evaluate::evaluate(.expr)
+#
+#   if (length(my_output) > 1) {
+#
+#     wrapped <- map(my_output[-1], function(x) knitr:::wrap(x, ...)) %>% str_c(collapse = "") %>% unlist()
+#
+#   } else {
+#
+#     wrapped <- ""
+#
+#   }
+#
+#   return(wrapped)
+#
+# }
+
+#' @export
+#' @imporFrom purrr map
+knit_print.demo_code <- function(x, ...) {
+
+  #x$wrapped <- map(x$evaluations, ~map_if(.x, length(.x) > 1, function(val) knitr:::wrap(val, ...) %>% str_c(collapse = "")))
+
+
+  x$wrapped <- map(x$evaluations, function(val) if (length(val) > 2) knitr:::wrap(val[[2]], ...))
+
+  to_print <- paste(unlist(x$print_string), unlist(x$wrapped)) %>% str_c(collapse = " ")
+
+  asis_output(to_print)
+
+  #to_print <- str_c(to_print, collapse = "<br>")
+
+    #knitr::asis_output(paste(attr(x, "print_string"), knitr:::wrap(x[[2]], ...)))
+
+  # } else {
+  #
+  #   knitr::asis_output(attr(x, "print_string"))
+  #
+  # }
+
 }
 
 
-print.demo_code <- function(x, ...) {
-
-  attr(x, "expression") %>%
-    eval() %>%
-    print()
-
-}
+# print.demo_code <- function(x, ...) {
+#
+# }
 
 
 
