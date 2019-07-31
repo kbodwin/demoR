01261457435bda3a4ddd1ec445366e8d352d8efc
Kelly N. Bodwin <kbodwin@calpoly.edu>
2019-07-31 06:29:08 -0700

    delete dev folder
    not for public consumption

--- dev/make_hex_skeleton.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-library(hexSticker)
-
-p <- ggplot(aes(x = mpg, y = wt), data = mtcars)
-p <- p + theme_void() + theme_transparent()
-
-sticker(p, package="", p_size=35, p_x = 1, p_y = 1, p_color = "#000000",
-        filename="./dev/sticker_base.png", h_color = "red",  h_fill = "#ffffff")
