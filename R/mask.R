1f25a3905ef1c6fdcdb879987ad1e64b6c304fed
hglanz <hglanz@calpoly.edu>
2019-08-11 23:01:53 -0700

    Substitute arg for mask()
    Original functionality of mask() is currently preserved, but the substitute argument is not fully working. I'm struggling with a weird warning about an IF condition having more than one element. Also, the result seems correct, but is not rendering?

--- R/mask.R
+++ R/mask.R
@@ -5,16 +5,31 @@
 #' @param ... Further formatting options, passed to \code{\link{txt_style}}
 #'
 #' @export
-mask <- function(.string, pattern, ...) {
+mask <- function(.string, pattern, substitute = NA, ...) {
 
-  mask_regexp(.string, fixed(pattern), ...)
+  mask_regexp(.string, fixed(pattern), substitute = substitute, ...)
 
 }
 
 #' @rdname mask
 #' @export
-mask_regexp <- function(.string, pattern, ...) {
+mask_regexp <- function(.string, pattern, substitute = NA, ...) {
 
-  hlt_regexp(.string, pattern, color = "transparent", ...)
+  if (is.na(substitute)) {
+    hlt_regexp(.string, pattern, color = "transparent", ...)
+  } else {
+    num_pattern <- str_count(.string, pattern)
 
+    if (length(substitute) == 1 & num_pattern == 1) { ## single replacement and single match?
+      sub_patterns <- substitute
+    } else if (length(substitute) == 1 & num_pattern > 1) { ## single replacement string to be enumerated?
+      sub_patterns <- paste(substitute, 1:num_pattern)
+    } else if (length(substitute) > 1) { ## recycle substitute to have num_pattern length
+      sub_patterns <- substitute[rep(1:length(substitute), length = num_pattern)]
+    }
+
+    .string <- Reduce(function (strx, replacex) str_replace(strx, pattern, replacex),
+                      c(.string, sub_patterns))
+    hlt_regexp(.string, fixed(sub_patterns), ...)
+  }
 }
