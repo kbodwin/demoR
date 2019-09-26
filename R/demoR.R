ebd5334cb916b4048c5663d5c2c8c4bf14eaf79f
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2019-09-26 20:03:23 +0100

    section titles should end in a colon (":") and declare "." as a global variable to quiet another R CMD check concern

--- R/demoR.R
+++ R/demoR.R
@@ -2,13 +2,16 @@
 #'
 #' The demoR package provides ...
 #'
-#' @section Highlight functions
+#' @section Highlight functions:
 #'
 #' The \code{hlt_*} functions add formatting to printed code.
 #'
-#' @section Demo functions
+#' @section Demo functions:
 #'
 #' The \code{demo_*} functions print and run the code.
 #'
 #' @docType package
 #' @name demoR
+NULL
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if (getRversion() >= "2.15.1")  utils::globalVariables(c("."))
